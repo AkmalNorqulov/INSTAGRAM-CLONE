db31544f79bc2d36444df2764e0133801a797413
Akmal <akmal9999007@gmail.com>
2025-11-15 10:06:59 +0500

    restart

--- users/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class CustomUser(AbstractUser):
-    profile_picture = models.ImageField(upload_to='profile_picture/', blank=True, null=True)
-    bio = models.TextField(max_length=250)
-    gender = models.CharField(max_length=10, blank=True, null=True)
