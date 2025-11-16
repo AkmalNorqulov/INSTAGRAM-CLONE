0704ee6a98425d715bfeb6b885da5efe6dafd246
Akmal <akmal9999007@gmail.com>
2025-11-16 09:31:39 +0500

    added LoginRequiredMixin to homeview

--- config/views.py
+++ config/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-class HomeView(View):
+from django.contrib.auth.mixins import LoginRequiredMixin
+class HomeView(LoginRequiredMixin, View):
     def get(self, request):
         return render(request, 'home.html')
