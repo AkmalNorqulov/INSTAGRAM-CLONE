b6fc02d275cdcdfbe1bc6197d61051ef92b2d8d6
Ozodbek <ozodbekusmonqulov7777@gmail.com>
2025-11-18 15:14:10 +0500

    update

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import UserProfile
 # Register your models here.
 
+# Django admin paneliga kirish uchun yozilgan class
+
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'date_joined', 'is_active')
