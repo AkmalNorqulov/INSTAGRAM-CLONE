785525c88b3b84a065a155bf6a048babb46d6c48
Ozodbek <ozodbekusmonqulov7777@gmail.com>
2025-11-20 14:38:55 +0500

    update

--- config/settings.py
+++ config/settings.py
@@ -119,3 +119,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.UserProfile'
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'home'
+AUTH_USER_MODEL = "users.UserProfile"
