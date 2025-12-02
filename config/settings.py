e838fd33ebd3ab21d6721cd8237d82e01c2ed0fe
Akmal <akmal9999007@gmail.com>
2025-12-02 15:02:35 +0500

    gg

--- config/settings.py
+++ config/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 STATICFILES_STORAGE = "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
