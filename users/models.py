f564fd5ebfb63dae26ba8f784aec2d6d32a93165
Akmal <akmal9999007@gmail.com>
2025-12-02 21:22:54 +0500

    fixed something

--- users/models.py
+++ users/models.py
@@ -8,8 +8,7 @@ from django.contrib.auth.models import AbstractUser
 # Foydalanuvchi profili uchun qilinga class va funksiyalar
 class UserProfile(AbstractUser):
     bio = models.TextField(max_length=300, blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True, default='profile_pictures/user.png')
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
 
     def __str__(self):
         return self.username
-        
